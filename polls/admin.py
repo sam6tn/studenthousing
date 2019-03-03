2a5b0e31b66ee200f4dab67506e52353ea7123cb
Hana Nur <hrn4ch@virginia.edu>
2019-03-03 13:55:51 -0500

    got rid of questions

--- polls/admin.py
+++ polls/admin.py
@@ -14,25 +14,5 @@ class PostAdmin(admin.ModelAdmin):
 
     search_fields = ['name']
 
-
-class ChoiceInline(admin.TabularInline):
-    model = Choice
-    extra = 3
-
-class QuestionAdmin(admin.ModelAdmin):
-    fieldsets = [
-        (None,               {'fields': ['question_text']}),
-        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
-    ]
-    inlines = [ChoiceInline]
-
-
-    list_display = ('question_text', 'pub_date', 'was_published_recently')
-
-    list_filter = ['pub_date']
-
-    search_fields = ['question_text']
-
-admin.site.register(Question, QuestionAdmin)
 admin.site.register(Post, PostAdmin)
 
