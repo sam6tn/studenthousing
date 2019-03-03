d9ad6be53e7200d98dea7b9a850671f776b2fbfd
Hana Nur <hrn4ch@virginia.edu>
2019-03-03 16:58:51 -0500

    changed polls/urls.py urls to django version needed for travis

--- polls/urls.py
+++ polls/urls.py
@@ -1,15 +1,14 @@
-from django.urls import path
-from django.http import HttpResponse
+from django.conf.urls import url
 
 from . import views
 
 
 app_name = 'housing'
 urlpatterns = [
-    path('', views.IndexView.as_view(), name='index'),
-    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
-    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
-    path('<int:question_id>/vote/', views.vote, name='vote'),
-    path('search/', views.SuggestView.as_view(), name='home'),
-    path('search/results/', views.ListView.as_view(), name='list'),
+    url(r'^', views.IndexView.as_view(), name='index'),
+    url(r'^<int:pk>/', views.DetailView.as_view(), name='detail'),
+    url(r'^<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+    url(r'^<int:question_id>/vote/', views.vote, name='vote'),
+    url(r'^search/', views.SuggestView.as_view(), name='home'),
+    url(r'^search/results/', views.ListView.as_view(), name='list'),
 ]
