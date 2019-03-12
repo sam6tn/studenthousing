4388478ea9c98a1e4d793ec5a0fec5880c18fc5c
sam6tn <sam6tn@users.noreply.github.com>
2019-03-12 17:33:25 -0400

    Required Login and Searching Options by name
    If user is not authenticated, redirects to login page with error message. Added simple filtering when searched by name.

--- polls/views.py
+++ polls/views.py
@@ -7,6 +7,8 @@ from django.views.generic import TemplateView
 from django.utils import timezone
 from django.views.generic import CreateView
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from django.contrib.messages import get_messages
 
 from .models import Post, Review
 
@@ -35,16 +37,16 @@ class SearchView(TemplateView):
             args = {'form': form, 'posts': posts}
             return render(request, self.template_name, args)
         else:
+            if (len(list(get_messages(request)))==0):
+                messages.error(request, 'Please Sign In!')
             return HttpResponseRedirect("/")
 
     def post(self, request):
         form = SuggestForm(request.POST)
         if form.is_valid():
-            form.save()
-            text = form.cleaned_data['search']
-            form = SuggestForm()
-
-            args = {'form': form, 'text': text}
+            search = form.cleaned_data['search']
+            #args = {'form': form, 'search': search}
+            request.session['search'] = search
         return HttpResponseRedirect("/search/results/")
 
 
@@ -52,10 +54,14 @@ class ListView(TemplateView):
     template_name = 'polls/list.html'
     def get(self, request):
         if request.user.is_authenticated:
-            form = SuggestForm()
-            posts = Post.objects.all()
-            args = {'form': form, 'posts': posts}
+            search = request.session.get('search')
+            #posts = Post.objects.all()
+            posts = Post.objects.filter(name__icontains=search)
+            #posts = Post.objects.order_by('search')
+            args = {'posts': posts}
             return render(request, self.template_name, args)
         else:
+            if (len(list(get_messages(request)))==0):
+                messages.error(request, 'Please Sign In!')
             return HttpResponseRedirect("/")
 
