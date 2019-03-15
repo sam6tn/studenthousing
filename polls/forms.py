1eb3f6de53aed8626adb03a27bb9fa6da13d1286
sam6tn <sam6tn@users.noreply.github.com>
2019-03-14 21:03:48 -0400

    Form updates

--- polls/forms.py
+++ polls/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from .models import Post, Review
 
 class SearchForm(forms.ModelForm):
-    search = forms.CharField(widget=forms.Textarea(attrs={'cols': 120, 'rows': 1}))
+    search = forms.CharField(widget=forms.TextInput(attrs={'size': 115,}))
     class Meta:
         model = Post
         fields = ('search',)
